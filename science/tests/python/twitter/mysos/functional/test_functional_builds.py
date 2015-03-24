b6cd91c3ab2fceac91c37f51ddb99626ee449519
Yan Xu <jyx@twitter.com>
2015-03-24 21:30:27 +0000

    Mysos: Move mysos_test_client from tests/ to src/ because new pants disallows it.
    - Just moved the files into /src because the new ./pants doesn't allow python_binary targets under tests/
    
    RB_ID=616482
    
    (sapling split of 60f394f39bebda43f7ae4b1b72db050dc266ca5e)

--- science/tests/python/twitter/mysos/functional/test_functional_builds.py
+++ science/tests/python/twitter/mysos/functional/test_functional_builds.py
@@ -2,5 +2,5 @@ from twitter.mysos.common.testing import build_and_execute_pex_target
 
 
 def test_testing_client_build():
-  build_and_execute_pex_target('tests/python/twitter/mysos/functional:mysos_test_client',
+  build_and_execute_pex_target('src/python/twitter/mysos/testing:mysos_test_client',
                                'dist/mysos_test_client.pex')
