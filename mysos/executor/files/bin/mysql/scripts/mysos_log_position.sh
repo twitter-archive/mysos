a205840e7665746d66ed766d663966d32c3fcc1d
Geoffrey Papilion <gpapilion@twitter.com>
2015-05-05 21:37:26 -0700

    fixes possible race condition when asking for log file and position

--- mysos/executor/files/bin/mysql/scripts/mysos_log_position.sh
+++ mysos/executor/files/bin/mysql/scripts/mysos_log_position.sh
@@ -5,16 +5,17 @@
 
 set -uxe
 
-slave_host=$1
-slave_port=$2
+slave_host="${1}"
+slave_port="${2}"
+mysql_command_string="mysql -u root -P ${slave_port} -h ${slave_host} -e \"show slave status\G;\""
 
-# Get the relay log file.
-relay_master_log_file=`mysql -u root -P $slave_port -h $slave_host -e "show slave status\G;" | \
-  grep Relay_Master_Log_File | awk '{print $2}'`
-
-# Get the relay log position.
-exec_master_log_pos=`mysql -u root -P $slave_port -h $slave_host -e "show slave status\G;"  | \
-  grep Exec_Master_Log_Pos | awk '{print $2}'`
+# Get the relay log file and position in one step.
+relay_master_log_file_and_position=$($mysq_command_string | \
+    awk '{if ($1 ~ "Relay_Master_Log_File"){log_file=$2}};
+    {if ($1 ~ "Exec_Master_Log_Pos"){log_pos=$2}};
+    END {if (length(log_file) != 0 && length(log_pos) != 0)
+    {print log_file","log_pos}
+    else{exit 1}}')
 
 # The output can be empty if the slave has not set up replication.
-echo $relay_master_log_file,$exec_master_log_pos
+printf '%s\n' $relay_master_log_file_and_position
